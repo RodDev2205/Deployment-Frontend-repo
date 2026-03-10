388a97088e34fb3ddf78f80bd737f5c49f5d84fc
Rod Angelo <vendiolaangelo2@gmail.com>
2026-03-10 12:09:14 +0800

    Print test

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({    alias: {
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-)
+})
